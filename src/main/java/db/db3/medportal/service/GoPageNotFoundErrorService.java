8779d67de408f3a74146835c0b4e0650838f9f81
Olzhik01 <180103273@stu.sdu.edu.kz>
2021-03-27 15:14:15 +0600

    #1 commit: Project source added

---
+++ src/main/java/db/db3/medportal/service/GoPageNotFoundErrorService.java
@@ -0,0 +1,21 @@
+package db.db3.medportal.service;
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
+
+import static db.db3.medportal.util.constants.PageName.PAGE_NOT_FOUND_ERROR_PAGE;
+
+public class GoPageNotFoundErrorService implements Service {
+    @Override
+    public void perform(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ParseException, SQLException {
+        RequestDispatcher requestDispatcher;
+        requestDispatcher = request.getRequestDispatcher(PAGE_NOT_FOUND_ERROR_PAGE);
+        requestDispatcher.forward(request,response);
+    }
+}
